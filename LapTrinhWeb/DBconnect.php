ed49faac5d3c652ba3b0a0d3203f1845c1d8e856
Thanh Tai <Thanh Tai@DESKTOP-6H9QI3M>
2019-05-14 18:52:57 +0700

    them new + sua database

--- LapTrinhWeb/DBconnect.php
+++ LapTrinhWeb/DBconnect.php
@@ -31,7 +31,7 @@ class control {
 $hostname = 'localhost:3306';
 $username = 'root';
 $password = '';
-$dbname = "test";
+$dbname = "test2";
 $conn = mysqli_connect($hostname, $username, $password,$dbname);
 if (!$conn) {
  die('Không thể kết nối: ' . mysqli_error($conn));

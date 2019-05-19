d3ef416cdb9dbbd7a57f5b285605092ce90e9be5
Thanh Tai <Thanh Tai@DESKTOP-6H9QI3M>
2019-05-19 09:43:16 +0700

    bug

--- motaikhoantietkiem.php
+++ motaikhoantietkiem.php
@@ -13,6 +13,9 @@
 	<title>Ngân Hàng STbank</title>
 	<link rel="stylesheet" type="text/css" href="index.css">
 	<link rel="icon" type="img/ico" href="hinh/logo.ico">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-4.0.0.css">	
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	
 	
 	<style>.account11{
 		border: solid;
@@ -26,7 +29,11 @@
 	color: white;
 	width: 290px;
 	min-height: 35px;
-	word-wrap: break-word;}</style>
+	word-wrap: break-word;
+		}
+		.account11 a{
+			color: aqua;
+		}
 </head>
 	
 <body bgcolor="lightblue" >

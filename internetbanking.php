af12098c3ad19d8ab20f1af479d3718ad0a1ee8a
Phạm Quang Khải <30576678+mrquangkhai98@users.noreply.github.com>
2019-05-27 01:07:19 +0700

    improve

--- internetbanking.php
+++ internetbanking.php
@@ -1,3 +1,4 @@
+
 <div id="khacall">
 <?php
 
@@ -5,3 +6,4 @@ require ("login.php");
 
 ?>
 </div>
+
--- login.php
+++ login.php
@@ -59,6 +59,69 @@ if (isset($_POST['login'])) {
 
 ?>
 
+<html lang="vi">
+<head>
+	<title>Login V19</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-50">
+				<form class="login100-form validate-form" method="post" name="loginform">
+					<span class="login100-form-title p-b-33">
+						Đăng nhập tài khoản Online Banking
+					</span>
+
+					<div class="wrap-input100 validate-input" data-validate = "Không được bỏ trống ID tài khoản">
+						<input class="input100" type="text" name="taikhoan" placeholder="Nhập tài khoản">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+
+					<div class="wrap-input100 rs1 validate-input" data-validate="Không được bỏ trống mật khẩu">
+						<input class="input100" type="password" name="password" placeholder="Nhập mật khẩu">
+						<span class="focus-input100-1"></span>
+						<span class="focus-input100-2"></span>
+					</div>
+
+					<div class="container-login100-form-btn m-t-20">
+						<button type="submit" name="login" value="Login" class="login100-form-btn">
+							Đăng nhập
+						</button>
+					</div>
+				</form>
+				<span class="text-danger"><?php if (isset($error_message)) { echo $error_message; } ?></span>
+			</div>
+		</div>
+	</div>
+
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/animsition/js/animsition.min.js"></script>
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	<script src="vendor/select2/select2.min.js"></script>
+	<script src="vendor/daterangepicker/moment.min.js"></script>
+	<script src="vendor/daterangepicker/daterangepicker.js"></script>
+	<script src="vendor/countdowntime/countdowntime.js"></script>
+	<script src="js/main.js"></script>
+
+<!--
 <div class="container" style="min-height: 500px;" align="center">	
 	<div class="row">
 		<div class="col-md-4 col-md-offset-4 well">
@@ -67,22 +130,23 @@ if (isset($_POST['login'])) {
 					<legend>Login</legend>						
 					<div class="form-group">
 						<label for="name">Email</label>
-						<input type="text" name="taikhoan" placeholder="Nhập Email của bạn" required class="form-control" />
+						<input type="text" name="taikhoan" placeholder="Nhập Email của bạn" required class="form-controla" />
 					</div>	
 					<div class="form-group">
 						<label for="name">Password</label>
-						<input type="password" name="password" placeholder="Nhập mật khẩu" required class="form-control" />
+						<input type="password" name="password" placeholder="Nhập mật khẩu" required class="form-controla" />
 					</div>	
 					<div class="form-group">
-						<input type="submit" name="login" value="Login" class="btn btn-primary" />
+						<button type="submit" name="login" value="Login" class="btn btn-primary" >Login</button>
 					</div>
 				</fieldset>
 			</form>
-			<span class="text-danger"><?php if (isset($error_message)) { echo $error_message; } ?></span>
+				
+		<span class="text-danger"><?php if (isset($error_message)) { echo $error_message; } ?></span>
+			
 		</div>
 	</div>
+-->
 
-	
-		
-
-</div>
+</body>
+</html>

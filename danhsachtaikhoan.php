897ccfa6ef269848e7209799e6a2abb6fd6cad72
Thanh Tai <Thanh Tai@DESKTOP-6H9QI3M>
2019-05-31 23:16:00 +0700

    ajax danhsachtaikhoan

--- danhsachtaikhoan.php
+++ danhsachtaikhoan.php
@@ -6,11 +6,31 @@
 }?>
 <?php require("DBconnect.php") ;
 	require_once("class_vs_function.php"); ?>
-	<?php 
-	    $sql = "SELECT * FROM taikhoan where id_khachhang=$_SESSION[id_khachhang]";
-		
+
+
+<script src="js/jquery.js">
+</script>
+<script> 
+ function boLoc(soTrang){
+	 soTrang = soTrang?soTrang:0 ;
 	
-	?>		
+	 var timkiem = $("#timkiem").val();
+	 var taikhoan = $("#taikhoan").val();
+	 var sotien = $("#sotien").val();
+	 var trangthai = $("#trangthai").val();
+	 $.ajax({
+		 type : "GET",
+		 url : "danhsachtaikhoan_xuly.php",
+		 data : "page=" + soTrang + "&timkiem="+ timkiem + "&taikhoan="+ taikhoan + "&sotien=" + sotien +
+		 "&trangthai=" + trangthai,
+		 success : function(dulieu){
+			 $("#noidung").html(dulieu);
+		 }
+	 })
+	 
+ }
+    
+</script>	
 
 
 
@@ -20,15 +40,15 @@
      	
            	  <table width="591" height="177"  align="center">
         	    <tr>
-					<input type="hidden"  name="ts" value="dstk">
+					
 				  <td> <strong> TÌM KIẾM THEO ID</strong></td>
-				   <td> <label><input type="input" name ="timkiem" onChange="form1.submit()"  value="<?php 
+				   <td> <label><input type="input" name ="timkiem" id="timkiem"  onKeyUp="boLoc()"  value="<?php 
 					   if (isset($_GET["timkiem"]) and $_GET["timkiem"] != "" ) echo $_GET["timkiem"]; ?>"> </label></td>
 				  </tr>
         	    <tr>
         	      <td><strong> TÀI KHOẢN </strong></td>
         	   <td><label>
-        	        <select name="taikhoan" id="taikhoan" onchange="form1.submit()"  >
+        	        <select name="taikhoan" id="taikhoan" onchange="boLoc()"  >
                              <option value="">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; </option>
         	 			<?php
 						$sql1 = "SELECT * FROM taikhoan where id_khachhang=$_SESSION[id_khachhang]"; ;
@@ -49,7 +69,7 @@
 				  <tr>
         	      <td><strong>TRẠNG THÁI </strong></td>
         	   <td><label>
-        	        <select name="trangthai" id="trangthai"  onchange="form1.submit()" >
+        	        <select name="trangthai" id="trangthai"  onchange="boLoc()" >
                              <option value="">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; </option>
 						
 						
@@ -66,7 +86,7 @@
 				<tr>
         	      <td><strong> SỐ TIỀN </strong></td>
         	   <td><label>
-        	        <select name="sotien" id="sotien" onchange="form1.submit()"  >
+        	        <select name="sotien" id="sotien" onchange="boLoc()"  >
                              <option value="">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; </option>
         	 			     <option value="1" <?php if (isset($_GET["sotien"]) and $_GET["sotien"] != "" and $_GET["sotien"] == 1) echo "selected ='selected'";?>> giảm dần </option>
 						    <option value="2" <?php if (isset($_GET["sotien"]) and $_GET["sotien"] != "" and $_GET["sotien"] == 2) echo "selected ='selected'";?>> tăng dần </option>
@@ -74,150 +94,6 @@
       	        </label></td>
       	      </tr> 
 	</table>
-</form>				
-<div align="center" >
-	
-     		 <h2>&nbsp;CHI TIẾT TÀI KHOẢN</h2>
-     		 <table width="560" border="1"  align="center">
-     		   <tr >
-     		     <th  width="105" scope="col">TÀI KHOẢN ID</th>      
-                 <th width="105" scope="col">TRANG THÁI</th>
-                 <th width="93" scope="col">NGÀY TẠO</th>
-                 <th width="101" scope="col">SỐ DƯ</th>
-                 <th width="144" scope="col">NỢ</th>
-                 
-      </tr>
-				 
-				 
-				<?php
-				 $truyendulieu ="";
-				 
-				 
-				 if (isset($_GET["taikhoan"]) and $_GET["taikhoan"] != "") { 
-					 $sql .=  " and id_taikhoan = $_GET[taikhoan]";
-					 $truyendulieu .= "&taikhoan=$_GET[taikhoan]";
-				 }
-				 
-				 if (isset($_GET["trangthai"]) and $_GET["trangthai"] != "") {
-				
-					 $sql .= " and trangthai = $_GET[trangthai]";
-					  $truyendulieu .= "&trangthai=$_GET[trangthai]";
-				 }
-				 
-				 
-			
-				  if (isset($_GET["timkiem"]) and $_GET["timkiem"] != "" ) { 
-					 $sql .=  " and taikhoanid like '%$_GET[timkiem]%'";
-					 $truyendulieu .= "&timkiem=$_GET[timkiem]";
-				 }
-				 
-				 
-				  if (isset($_GET["sotien"]) and $_GET["sotien"] != "") {
-					 if ($_GET["sotien"] == 1) {
-						 $sql .=  " order by sodu desc";
-					 $truyendulieu .= "&sotien=$_GET[sotien]";
-					 }
-					 if ($_GET["sotien"] == 2) {
-						 $sql .=  " order by sodu ASC";
-					 $truyendulieu .= "&sotien=$_GET[sotien]";
-					 }
-				 }
-				 
-				 
-				 ?> 
-				 
-				 <?php 
-	
-	
-	
-  $kq = $control->query($sql);
-  $tsp = mysqli_num_rows($kq);
-  $sd = 5 ;
-  
-  
-  $tst = ceil($tsp/$sd);
-  
-   if ($tsp != 0 ) {
-  
-
-  if (isset($_GET["page"])){
-	  $page = $_GET["page"] ;
-	  $gr =  ceil ($page / $sn);
-	  }
-	  else {
-		  $gr = $page = 1 ;}
-		  
-		  
-	$vitri = ($page - 1) * $sd ;
-	
-			 
-	$sql.= " limit $vitri , $sd ";
-	$kq = $control->query($sql);
-	
-	$dem = $vitri+1 ;
-  ?>  
-  <?php
-				 echo "$sql";
-   while($taikhoan = $control->fetch_arr($kq)){
-					 
-					 if ($taikhoan["trangthai"] == 2) $trangthai = "đang hoạt động" ;
-					 else $trangthai = "tạm ngưng";
-					 
-					if(isset($_GET["timkiem"]))$tais=highlightKeywords($taikhoan["taikhoanid"],$_GET["timkiem"]);
-	                else $tais = $taikhoan["taikhoanid"];
-	   
-	   
-					 echo "
-                   <tr>
-                    <td>&nbsp;$tais</td>
-                    <td>&nbsp;$trangthai</td>
-                    <td>&nbsp;$taikhoan[ngaytao]</td>
-                    <td>&nbsp;$taikhoan[sodu]</td>
-                    <td>&nbsp;$taikhoan[no]</td>
-                   
-        
-                  </tr>";
-	  }
-				 
-				 
-				 
-				 
-				 ?>				 
-				 
-				 
-   		   </table>
-	
-	<div class="pagination clearfix" >	
-	<?php 
-  if ($page != 1 and $page != 2) {
-  $dau = $page-2 ;
-   
-  }
-  else $dau = 1;
-	$cuoi = $page + 2; 
-  if ($cuoi > $tst) $cuoi = $tst ;
-  
-  
-  ?>
-  <?php  if ($page != 1 ){
-	  ?>
-      <a href="acctrangchu.php?ts=dstk&page=<?php echo 1 ; echo $truyendulieu;?>"> << </a>
-      
-	  <?php 
-  }
-  
-  for($i=$dau;$i<=$cuoi;$i++){
-	     if ($page == $i) echo "<strong> $i </strong> &nbsp;";
-		 else {
-			 ?>
-	             
-	       <a href="acctrangchu.php?ts=dstk&page=<?php echo $i ; echo $truyendulieu;?> "><?php echo $i ; ?> &nbsp;</a>
-            
-     <?php }
-  }
-   if ($page != $tst ) {?> <a href="acctrangchu.php?ts=dstk&page=<?php echo $tst ;echo $truyendulieu; ?>"> >> </a>  <?php 
-   }
-  ?></p>
-	</div>
-<?php } ?>
+</form>	
+<div id="noidung"></div>
 	
---
+++ danhsachtaikhoan_xuly.php
@@ -0,0 +1,153 @@
+ <?php @session_start(); ?>
+<?php require("DBconnect.php") ;
+      require_once("class_vs_function.php");
+?>
+		
+<?php 
+	    $sql = "SELECT * FROM taikhoan where id_khachhang=$_SESSION[id_khachhang]";
+	?>		
+<div align="center" >
+	
+     		 <h2>&nbsp;CHI TIẾT TÀI KHOẢN</h2>
+     		 <table width="560" border="1"  align="center">
+     		   <tr >
+     		     <th  width="105" scope="col">TÀI KHOẢN ID</th>      
+                 <th width="105" scope="col">TRANG THÁI</th>
+                 <th width="93" scope="col">NGÀY TẠO</th>
+                 <th width="101" scope="col">SỐ DƯ</th>
+                 <th width="144" scope="col">NỢ</th>
+                 
+      </tr>
+				 
+				 
+				<?php
+				 $truyendulieu ="";
+				 
+				 
+				 if (isset($_GET["taikhoan"]) and $_GET["taikhoan"] != "") { 
+					 $sql .=  " and id_taikhoan = $_GET[taikhoan]";
+					 $truyendulieu .= "&taikhoan=$_GET[taikhoan]";
+				 }
+				 
+				 if (isset($_GET["trangthai"]) and $_GET["trangthai"] != "") {
+				
+					 $sql .= " and trangthai = $_GET[trangthai]";
+					  $truyendulieu .= "&trangthai=$_GET[trangthai]";
+				 }
+				 
+				 
+			
+				  if (isset($_GET["timkiem"]) and $_GET["timkiem"] != "" ) { 
+					 $sql .=  " and taikhoanid like '%$_GET[timkiem]%'";
+					 $truyendulieu .= "&timkiem=$_GET[timkiem]";
+				 }
+				 
+				 
+				  if (isset($_GET["sotien"]) and $_GET["sotien"] != "") {
+					 if ($_GET["sotien"] == 1) {
+						 $sql .=  " order by sodu desc";
+					 $truyendulieu .= "&sotien=$_GET[sotien]";
+					 }
+					 if ($_GET["sotien"] == 2) {
+						 $sql .=  " order by sodu ASC";
+					 $truyendulieu .= "&sotien=$_GET[sotien]";
+					 }
+				 }
+				 
+				 
+				 ?> 
+				 
+				 <?php 
+	
+	
+	
+  $kq = $control->query($sql);
+  $tsp = mysqli_num_rows($kq);
+  $sd = 5 ;
+  
+  
+  $tst = ceil($tsp/$sd);
+  
+   if ($tsp != 0 ) {
+  
+
+  if (isset($_GET["page"]) and $_GET["page"] != 0){
+	  $page = $_GET["page"] ;
+	 
+	  }
+	  else {
+		  $gr = $page = 1 ;}
+		  
+		  
+	$vitri = ($page - 1) * $sd ;
+	
+			 
+	$sql.= " limit $vitri , $sd ";
+	$kq = $control->query($sql);
+	
+	$dem = $vitri+1 ;
+  ?>  
+  <?php
+				 echo "$sql";
+   while($taikhoan = $control->fetch_arr($kq)){
+					 
+					 if ($taikhoan["trangthai"] == 2) $trangthai = "đang hoạt động" ;
+					 else $trangthai = "tạm ngưng";
+					 
+					if(isset($_GET["timkiem"]))$tais=highlightKeywords($taikhoan["taikhoanid"],$_GET["timkiem"]);
+	                else $tais = $taikhoan["taikhoanid"];
+	   
+	   
+					 echo "
+                   <tr>
+                    <td>&nbsp;$tais</td>
+                    <td>&nbsp;$trangthai</td>
+                    <td>&nbsp;$taikhoan[ngaytao]</td>
+                    <td>&nbsp;$taikhoan[sodu]</td>
+                    <td>&nbsp;$taikhoan[no]</td>
+                   
+        
+                  </tr>";
+	  }
+				 
+				 
+				 
+				 
+				 ?>				 
+				 
+				 
+   		   </table>
+	
+	<div class="pagination clearfix" >	
+	<?php 
+  if ($page != 1 and $page != 2) {
+  $dau = $page-2 ;
+   
+  }
+  else $dau = 1;
+	$cuoi = $page + 2; 
+  if ($cuoi > $tst) $cuoi = $tst ;
+  
+  
+  ?>
+  <?php  if ($page != 1 ){
+	  ?>
+      <a href="Javascript:;" onClick="boLoc(1)"> << </a>
+      
+	  <?php 
+  }
+  
+  for($i=$dau;$i<=$cuoi;$i++){
+	     if ($page == $i) echo "<strong> $i </strong> &nbsp;";
+		 else {
+			 ?>
+	             
+	       <a href="Javascript:;"  onClick="boLoc(<?php echo $i ?>)" ><?php echo $i ; ?> &nbsp;</a>
+            
+     <?php }
+  }
+   if ($page != $tst ) {?> <a href="Javascript:;" onClick="boLoc(<?php echo $tst ?>)" > >> </a>  <?php 
+   }
+  ?></p>
+	</div>
+<?php } ?>
